3252ead3a131419004716157c65829c6a464f1d0
sravangorati2001 <sravangorati2001@gmail.com>
2023-09-20 23:51:19 -0700

    frontend - server integration

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,16 @@
-import Image from 'next/image'
+"use client"
+import React , {useEffect,useState} from 'react'
+
 
 export default function Home() {
+  useEffect(()=>{
+    const getData = async ()=> {
+      const query = await fetch("http://localhost:8080/home");
+      const resp = await query.json();
+      console.log('response: ',resp);
+    }
+    getData();
+  },[]);
   return (
     <main>Hello World.....!</main>
   )
