d85b6ec30d9a73d93d2d68a0338ea4a1b72dd565
Mahendra Chittupolu <mahendra@Mahendras-MacBook-Air.local>
2023-11-11 12:58:38 -0800

    login route

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -25,5 +25,36 @@ router.post('/signup', async (req, res) => {
   }
 })
 
+router.post("/login", async(req, res) => {
+    try{
+        console.log(req.params);
+        email = req.body.email;
+        password = req.body.password;
+        const users = await User.find({email : email });
+        console.log(users);
+        if(users.length === 0){
+            res.json({
+                message: 'user not found',
+                status : HTTP_STATUS_CODES.NOT_FOUND
+            })
+        }
+        else{
+            data = { email : req.body.email , fullname : users.fullname}
+            res.json({
+                message: 'user found',
+                status : HTTP_STATUS_CODES.OK,
+                data : JSON.stringify(data)
+            })
+        }
+
+    }
+    catch (error){
+            console.error('Error loggin in user', error);
+            res.json({
+                message : 'Uff..Somethin went wrong..Contact admin',
+                status: HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR
+            })
+    }
+})
 router.get
 module.exports = router;
