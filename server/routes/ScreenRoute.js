cc047b459bc3155ccc1c76217f95118a36922094
sravangorati2001 <sravangorati2001@gmail.com>
2023-11-14 23:57:06 -0800

    added new routes: movie and screen. and added api's

---
+++ server/routes/ScreenRoute.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const router = express.Router();
+const Screen = require('../models/ScreenModel');
+const { upload } = require('../Helpers/S3');
+
+router.post('/add', async (req, res) => {
+     try {
+        console.log(req.body);
+        const newScreen = new Screen({
+           
+        });
+
+        // Save the user to the database
+        await newScreen.save();
+        res.json({ message: "Added movie successfully", status: HTTP_STATUS_CODES.OK });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+router.get('/list/all', async (req, res) => {
+     try {
+      
+        // Save the user to the database
+        const screen = await Screen.find();
+        res.json({ message: "Added movie successfully", status: HTTP_STATUS_CODES.OK,screens:screen });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
